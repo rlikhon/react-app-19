de083f77ffc037954d361882ef69d673a6f341f9
Rezoan <rlikhon@gmail.com>
2025-08-25 13:02:31 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+       <h1>Likhon is here.</h1>
+      <p>We are learning React js with version 19 .</p>
+    </>
+  )
+}
+
+export default App
