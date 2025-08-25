b9a1b391349a376adcf48485952d006fa196a69d
Rezoan <rlikhon@gmail.com>
2025-08-25 17:34:10 +0600

    use Transition effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useTransition } from 'react';
+import Button from 'react-bootstrap/Button';
 import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
+  const [pending, startTransition] = useTransition();
 
+  const handleButton = () => {
+    console.log("Button Clicked")     
+    startTransition(async () => {
+      await new Promise((resolve) => { setTimeout(resolve, 3000) })     
+    })
+  }
   return (
     <>
-       <h1>Likhon is here.</h1>
+       <h1>Use react useTransition</h1>
       <p>We are learning React js with version 19 .</p>
+
+      <br></br>
+      {
+        pending? 
+        <img src="https://cdn.dribbble.com/userupload/22076800/file/original-8e7ce77dec0edaf0105e8287038f6e60.gif" alt="React Logo" width="150" />
+        : null
+      }
+      
+      <br></br>
+      <Button variant="warning" disabled={pending} onClick={handleButton}>Click Me</Button>
     </>
   )
 }
