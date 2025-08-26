b24a5c563a444d050c0db9126dd3c047a1ab490c
Rezoan <rlikhon@gmail.com>
2025-08-26 12:38:47 +0600

    react fragment

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState, useTransition } from "react";
 import Button from "react-bootstrap/Button";
 import "./App.css";
-import AddUser from "./AddUser";
-import DisplayUser from "./DisplayUser";
 
 function App() {
-  const [user, setUser] = useState("");
-  const [pending, startTransition] = useTransition();
-
-  const handleButton = () => {
-    console.log("Button Clicked");
-    startTransition(async () => {
-      await new Promise((resolve) => {
-        setTimeout(resolve, 3000);
-      });
-    });
-  };
   return (
     <>
-      <h1>We are learning React js with version 19 .</h1>
-
-      <br></br>
-      <div
-        style={{ border: "2px solid black", margin: "20px", padding: "20px" }}
-      >
-        <h3>Share State Data {user}</h3>
-        <AddUser setuser={setUser} />
-        <DisplayUser user={user} />
+      <header className="App-header"> 
+        <h3>React Fragment</h3>
+      </header>
+      <div className="App">
+        <Apple />
+        <Apple />
+        <Apple />
       </div>
     </>
   );
 }
 
 export default App;
+
+
+function Apple()
+{
+  return(
+    <>
+      <h1>Hello</h1>
+    </>
+  );  
+}
