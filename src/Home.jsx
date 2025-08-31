a5686383e51a213ff5a2ca8978e14c561537d423
Rezoan <rlikhon@gmail.com>
2025-09-01 00:07:04 +0600

    react 19 URL Fetch

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,9 @@
 function Home() {
   return (
     <>
+      <div className="bg-blue-200 p-4 text-cyan-600 border-red-500 border-4 m-4">
       <h1 className="text-3xl font-bold underline">    Hello world! Header  </h1>
+      </div>
     </>
   );
 }
--- src/Users.jsx
+++ src/Users.jsx
@@ -1,30 +1,35 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router";
 
 
 export default function Usres() {
-  const usersData = [
-    { id: 1, name: "Alice", email: "alice@gmail.com" },
-    { id: 2, name: "Bob", email: "bob@gmail.com" },
-    { id: 3, name: "Charlie", email: "charlie@gmail.com" },
-    { id: 4, name: "David", email: "david@gmail.com" },
-    { id: 5, name: "Eve", email: "eve@gmail.com" },
-    { id: 6, name: "Frank", email: "frank@gmail.com" },
-    { id: 7, name: "Grace", email: "grace@gmail.com" },
-    { id: 8, name: "Heidi", email: "heidi@gmail.com" },
-    { id: 9, name: "Ivan", email: "ivan@gmail.com" },
-    { id: 10, name: "Judy", email: "judy@gmail.com" }
-  ]
+  const [usersData, setUsersData] = useState([]);
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  let url = "https://dummyjson.com/users";
+
+  async function fetchUsers() {
+    const response = await fetch(url);
+    const json = await response.json();
+    setUsersData(json.users);
+  }
+
+    console.log("usersData", usersData);
+
+
   return (
     <>
       <div style={{ backgroundColor: "Green", padding: "20px", textAlign: "center" }}>
         <h3>Users List</h3>
         <div style={{ display: "flex", justifyContent: "center", flexWrap: "wrap" }}>
-          {usersData.map((user) => (
+          {usersData.map((user, index) => (
             <Link to={`/users/${user.id}`} style={{ textDecoration: "none", color: "black" }}>
 
-            <div key={user.id} style={{ border: "1px solid black", borderRadius: "5px", padding: "10px", margin: "10px", width: "200px", backgroundColor: "lightgray" }}>
-              <h4>{user.name}</h4>
-              <p>{user.email}</p>
+            <div key={index} style={{ border: "1px solid black",overflow: "auto", borderRadius: "5px", padding: "10px", margin: "10px", width: "200px", backgroundColor: "lightgray" }}>
+              <h4>{user.firstName}</h4>
+              <p className="text-sm">{user.email}</p>
             </div>
             </Link>
           ))}
