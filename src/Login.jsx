9bac460b512e6abea7364e7b2bd5ce9a58299ffd
Rezoan <rlikhon@gmail.com>
2025-09-06 00:14:16 +0600

    aReact 19 validation

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,12 +1,70 @@
-
+import { useActionState } from "react";
 
 function Login() {
+  const handleLogin = (prevData, formData) => {
+    //e.preventDefault();
+    let username = formData.get("username");
+    let password = formData.get("password");
+    let regex = /^[A-Za-z0-9]+$/i;
+
+
+    if (username.length < 5) {
+      return {error: "Username must be at least 5 characters long"};
+    } else if (!regex.test(password)) {
+      return {error: "Password must contain at least one letter or number"};
+    } else if (password.length < 6) {
+      return {error: "Password must be at least 6 characters long"};
+    }else {
+      return {message: "Login Successful"};
+    }
+  }
+
+  const [data, action, pending] = useActionState(handleLogin);
+  
+  console.log("Data:", data);
+  
   return (
     <>
-      <div style={{ backgroundColor: "Green", padding: "20px", textAlign: "center" }}>
-        <h3>Login Component</h3>
+      <div
+        style={{
+          backgroundColor: "#dee2e6",
+          padding: "20px",
+          textAlign: "center",
+        }}
+      >
+        <h3>Login</h3>
+        <form action={action}>
+        {
+            data && data.error && (
+              <div style={{ color: "red", marginBottom: "10px" }}>
+                {data.error}
+              </div>
+            )
+            
+          }
+          { data && data.message && (
+              <div style={{ color: "green", marginBottom: "10px" }}>
+                {data.message}
+              </div>
+            )
+          }
+          {
+            pending && (
+              <div style={{ color: "blue", marginBottom: "10px" }}>
+                Processing...
+              </div>
+            )
+          }
+
+          <div style={{ marginBottom: "10px" }}>
+            <input defaultValue={data?.username} type="text" name="username" placeholder="Username" />
+          </div>
+          <div style={{ marginBottom: "10px" }}>
+            <input type="password" defaultValue={data?.password} name="password" placeholder="Password" />
+          </div>
+          <button disabled={data?.error}>Login</button>
+        </form>
       </div>
-      
     </>
   );
 }
